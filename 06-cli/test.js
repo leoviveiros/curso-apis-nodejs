e0310ed9710b623f3bc3b36ff49ab6e17b0687cf
Leonardo Pinto <leoviveiros@gmail.com>
2022-02-05 14:34:25 -0300

    refatoracao testes

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -10,23 +10,33 @@ const HERO_FLASH = {
 
 describe('Suite de manipulacao de herois', () => {
 
-    it('deve pesquisar um heroi, usando arquivos', async () => {
-        const expected = HERO_FLASH;
-        const actual = await database.buscarPorId(expected.id);
+    describe('listar e buscar herois', () => {
+        beforeEach(async () => {
+            await database.escreverArquivo([HERO_FLASH]);
+        });
 
-        deepEqual(actual, expected);
+        it('deve pesquisar um heroi, usando arquivos', async () => {
+            const expected = HERO_FLASH;
+            const actual = await database.buscarPorId(expected.id);
+
+            deepEqual(actual, expected);
+        });
+
+        it('deve pesquisar listar os herois, usando arquivos', async () => {
+            const expected = [HERO_FLASH];
+            const actual = await database.listar();
+
+            deepEqual(actual, expected);
+        });
     });
 
-    it('deve pesquisar listar os herois, usando arquivos', async () => {
-        const expected = [HERO_FLASH];
-        const actual = await database.listar();
-
-        deepEqual(actual, expected);
+    afterEach(async () => {
+        await database.escreverArquivo([]);
     });
 
     it('deve cadastrar um heroi, usando arquivos', async () => {
         const expected = { id: 2, name: 'Batman', power: 'Money' };
-        
+
         await database.cadastrar(expected);
 
         const actual = await database.buscarPorId(expected.id);
